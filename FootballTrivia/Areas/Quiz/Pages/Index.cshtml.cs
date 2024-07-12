af22227ab69905bafae6c8f71c45a12738301173
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-07-12 17:03:33 +0100

    Add quiz area

---
+++ FootballTrivia/Areas/Quiz/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FootballTrivia.Areas.Quiz.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
