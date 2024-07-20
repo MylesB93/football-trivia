c91b99a8c4a80f61b2f93149255d2a6bf70263bc
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-07-20 12:11:48 +0100

    Change colour of buttons to indicate correctness

--- FootballTrivia/wwwroot/js/site.js
+++ FootballTrivia/wwwroot/js/site.js
@@ -8,9 +8,15 @@ var correctAnswer = questions[currentQuestion][1][0]
 var answers = questions[currentQuestion][1]
 var randomisedAnswers = answers.sort(() => Math.random() - 0.5)
 
-document.getElementById('answer-0').innerHTML = randomisedAnswers[0]
-document.getElementById('answer-1').innerHTML = randomisedAnswers[1]
-document.getElementById('answer-2').innerHTML = randomisedAnswers[2]
+var answer0 = document.getElementById('answer-0')
+var answer1 = document.getElementById('answer-1')
+var answer2 = document.getElementById('answer-2')
+
+answer0.innerHTML = randomisedAnswers[0]
+answer1.innerHTML = randomisedAnswers[1]
+answer2.innerHTML = randomisedAnswers[2]
+
+var elemList = [answer0, answer1, answer2]
 
 function checkAnswer(element) {
 	var answerCheck = document.getElementById('answer-check')
@@ -26,4 +32,17 @@ function checkAnswer(element) {
 		answerCheck.style.color = red
 	}
 	answerCheck.style.display = 'block'
+	isCorrect(elemList)
+	// TODO: Disable other buttons so user can't change answer
+}
+
+function isCorrect(elementList) {
+	elementList.forEach(element => {
+		element.classList.remove('btn-primary')
+		if (correctAnswer == element.innerHTML) {
+			element.classList.add('btn-success')
+		} else {
+			element.classList.add('btn-danger')
+		}
+    })	
 }
