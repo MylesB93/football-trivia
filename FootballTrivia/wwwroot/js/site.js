1a6f5df2bece2cfdc10ca39fe884c25c5857161c
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-08-06 21:34:41 +0100

    Move js logic to script file

--- FootballTrivia/wwwroot/js/site.js
+++ FootballTrivia/wwwroot/js/site.js
@@ -1,47 +1,72 @@
-﻿var questions = window.questions;
-var currentQuestion = 0;
+﻿//# region Answer round logic
+// WIP: Quiz section of game needs implementing
+//var questions = window.questions;
+//var currentQuestion = 0;
 
-document.getElementById('question').innerHTML = questions[currentQuestion][0]
+//document.getElementById('question').innerHTML = questions[currentQuestion][0]
 
-var correctAnswer = questions[currentQuestion][1][0]
-var answers = questions[currentQuestion][1]
-var randomisedAnswers = answers.sort(() => Math.random() - 0.5)
+//var correctAnswer = questions[currentQuestion][1][0]
+//var answers = questions[currentQuestion][1]
+//var randomisedAnswers = answers.sort(() => Math.random() - 0.5)
 
-var answer0 = document.getElementById('answer-0')
-var answer1 = document.getElementById('answer-1')
-var answer2 = document.getElementById('answer-2')
+//var answer0 = document.getElementById('answer-0')
+//var answer1 = document.getElementById('answer-1')
+//var answer2 = document.getElementById('answer-2')
 
-answer0.innerHTML = randomisedAnswers[0]
-answer1.innerHTML = randomisedAnswers[1]
-answer2.innerHTML = randomisedAnswers[2]
+//answer0.innerHTML = randomisedAnswers[0]
+//answer1.innerHTML = randomisedAnswers[1]
+//answer2.innerHTML = randomisedAnswers[2]
 
-var elemList = [answer0, answer1, answer2]
+//var elemList = [answer0, answer1, answer2]
 
-function checkAnswer(element) {
-	var answerCheck = document.getElementById('answer-check')
-	var green = '#12c712'
-	var red = '#ff0000'
-	
-	var answer = element.innerHTML
-	if (answer == correctAnswer) {
-		answerCheck.innerHTML = "That's correct!"
-		answerCheck.style.color = green
-	} else {
-		answerCheck.innerHTML = "That's incorrect!"
-		answerCheck.style.color = red
-	}
-	answerCheck.style.display = 'block'
-	isCorrect(elemList)
-}
+//function checkAnswer(element) {
+//	var answerCheck = document.getElementById('answer-check')
+//	var green = '#12c712'
+//	var red = '#ff0000'
 
-function isCorrect(elementList) {
-	elementList.forEach(element => {
-		element.classList.remove('btn-primary')
-		element.onclick = null
-		if (correctAnswer == element.innerHTML) {
-			element.classList.add('btn-success')
-		} else {
-			element.classList.add('btn-danger')
+//	var answer = element.innerHTML
+//	if (answer == correctAnswer) {
+//		answerCheck.innerHTML = "That's correct!"
+//		answerCheck.style.color = green
+//	} else {
+//		answerCheck.innerHTML = "That's incorrect!"
+//		answerCheck.style.color = red
+//	}
+//	answerCheck.style.display = 'block'
+//	isCorrect(elemList)
+//}
+
+//function isCorrect(elementList) {
+//	elementList.forEach(element => {
+//		element.classList.remove('btn-primary')
+//		element.onclick = null
+//		if (correctAnswer == element.innerHTML) {
+//			element.classList.add('btn-success')
+//		} else {
+//			element.classList.add('btn-danger')
+//		}
+//    })
+//}
+
+var speedAnswerInput = document.getElementById('speed-answer');
+var startGameBtn = document.getElementById('start-game');
+var answers = window.answers;
+var points = 0;
+
+speedAnswerInput.addEventListener('keyup', function () {
+	var answer = this.value;
+	if (answers.includes(answer.toLowerCase())) {
+		alert('Correctomundo!!!!'); // TODO: Display some kind of message indicating correct answer
+		this.value = "";
+		var index = answers.indexOf(answer);
+		if (index > -1) {
+			answers.splice(index, 1);
+			console.log(answers)
 		}
-    })	
-}
+	}
+});
+
+startGameBtn.addEventListener('click', function () {
+	speedAnswerInput.style.display = 'block';
+	this.style.display = 'none';
+});
