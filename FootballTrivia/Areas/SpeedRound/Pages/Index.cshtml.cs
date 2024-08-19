d0ad72b63043d73c3c46dc956cb942a335b40c0c
Myles Broomes <mylesbroomes@hotmail.co.uk>
2024-08-19 17:20:43 +0100

    Address warnings

--- FootballTrivia/Areas/SpeedRound/Pages/Index.cshtml.cs
+++ FootballTrivia/Areas/SpeedRound/Pages/Index.cshtml.cs
@@ -5,24 +5,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FootballTrivia.Areas.SpeedRound.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly IQuizService _quizService;
-        private readonly IUserService _userService;
+	public class IndexModel : PageModel
+	{
+		private readonly IQuizService _quizService;
+		private readonly IUserService _userService;
 
-        public List<string>? Standings;
-        public int HighScore { get; set; } // TODO: Remember to update this whenever the user gets a new high score
+		public List<string?>? Standings;
+		public int HighScore { get; set; } // TODO: Remember to update this whenever the user gets a new high score
 
-        public IndexModel(IQuizService quizService, IUserService userService)
-        {
-            _quizService = quizService;
-            _userService = userService;
-        }
+		public IndexModel(IQuizService quizService, IUserService userService)
+		{
+			_quizService = quizService;
+			_userService = userService;
+		}
 
-        public async Task OnGetAsync(string lid, string year = "2023")
-        {
-            Standings = await _quizService.GetSpeedRoundQuestionsAsync(lid, year);
-            HighScore = _userService.GetHighScore(User.Identity.Name);
-        }
-    }
+		public async Task OnGetAsync(string lid, string year = "2023")
+		{
+			Standings = await _quizService.GetSpeedRoundQuestionsAsync(lid, year); //TODO: Figure out why warning is being thrown here
+
+			if (User?.Identity?.Name != null)
+				HighScore = _userService.GetHighScore(User.Identity.Name);
+		}
+	}
 }
